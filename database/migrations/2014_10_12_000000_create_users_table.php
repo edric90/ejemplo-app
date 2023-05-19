56135ce95f6ac71ee0e2e0a9a77ae2d1ea8b2fc8
edric90 <edric90@gmail.com>
2023-05-19 10:14:44 -0400

    3 mod

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('email')->unique();
+            $table->string('email',250)->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('address');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
