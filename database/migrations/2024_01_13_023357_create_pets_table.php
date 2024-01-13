f2acc8f0a66a740173d1558c8132ab9db829f9fb
Edison Ricardo Caero Flores <edri90@gmail.com>
2024-01-12 23:10:25 -0400

    creacion de tablas

---
+++ database/migrations/2024_01_13_023357_create_pets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->string('color',20);
+            $table->string('gender',10);            
+            $table->integer('age');
+            $table->string('type',50);
+            $table->unsignedBigInteger('clients_id');
+            $table->foreign('clients_id')
+            ->references('id')
+            ->on('clients')
+            ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pets');
+    }
+};
