4a1515422c23db8bc2600f1e47317112f239fa0b
Edison Ricardo Caero Flores <edri90@gmail.com>
2024-02-01 01:27:49 -0400

    Agrupando controladores

--- routes/api.php
+++ routes/api.php
@@ -21,17 +21,44 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::controller(ClientController::class)->group(function () 
+{
+    Route::get("/clients", "index");
+    Route::post("/clients", "store");
+    Route::get("/clients/{clientId}", "show");
+    Route::patch("/clients/{clientId}", "update");
+    Route::delete("/clients/{clientId}", "destroy");
+});
+/* todo esto se agrupo en un solo grupo de rutas
 Route::get("/clients",[ClientController::class,"index"]);
 Route::post("/clients",[ClientController::class,"store"]);
 Route::get("/clients/{clientId}",[ClientController::class,"show"]);
 Route::patch("/clients/{clientId}",[ClientController::class,"update"]);
 Route::delete("/clients/{clientId}",[ClientController::class,"destroy"]);
+*/
 
+Route::controller(PetController::class)->group(function () 
+{
+    Route::get("/pets","index");
+    Route::post("/pets","store");
+    Route::get("/pets/{pet_id}","show");
+    Route::patch("/pets/{pet_id}","update");
+    Route::delete("/pets/{pet_id}","destroy");
+});
+/*
 Route::get("/pets",[PetController::class,"index"]);
 Route::post("/pets",[PetController::class,"store"]);
 Route::get("/pets/{pet_id}",[PetController::class,"show"]);
 Route::patch("/pets/{pet_id}",[PetController::class,"update"]);
 Route::delete("/pets/{pet_id}",[PetController::class,"destroy"]);
+*/
 
+Route::controller(PetController::class)->group(function () 
+{
+    Route::get("/client-pet","index");
+    Route::get("/client-pet/{clientId}","show");
+});
+/*
 Route::get("/client-pet",[ClientPetController::class,"index"]);
 Route::get("/client-pet/{clientId}",[ClientPetController::class,"show"]);
+*/
