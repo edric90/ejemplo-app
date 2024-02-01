7fcf7a326be612575657646ccb8f3410ab05e66d
Edison Ricardo Caero Flores <edri90@gmail.com>
2024-02-01 00:21:43 -0400

    Api Mascotas creadas

--- app/Http/Controllers/ClientPetController.php
+++ app/Http/Controllers/ClientPetController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Client;
 use App\Models\Pet;
+use Illuminate\Support\Facades\DB;
 
 class ClientPetController extends Controller
 {
@@ -13,9 +14,23 @@ class ClientPetController extends Controller
      */
     public function index()
     {
-        //$client = Client::all();
-        $client = Client::with('pets')->get();
-        return response()->json($client);
+        //$client = Client::all(); //consulta de todos los clientes
+        //$client = Client::with('pets')->get(); //consulta de clientes y mascotas
+
+        // Consulta tabla (sql) clientes y mascotas
+        // $client = DB::table("clients")->Join("pets","clients.id","=","pets.client_id","inner")
+        // ->select("clients.*","pets.*")
+        // ->get();
+        //return response()->json($client); // retorna datos clientes y mascotas
+
+        //$pet = Pet::with('client')->get(); // retorna mascotas y clientes
+        // Consulta tabla (sql) mascotas y clientes
+        $pet = DB::table("pets")->Join("clients","clients.id","=","pets.client_id","inner")
+        ->select("pets.*","clients.*")
+        ->get();
+
+        return response()->json($pet); // retorna datos clientes y mascotas
+
     }
 
     /**
